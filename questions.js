c19de1590d99715f18423d94f4e7d693f3b2b7fc
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2023-05-28 03:32:23 +0100

    Quiz_app

---
+++ questions.js
@@ -0,0 +1,76 @@
+export default [
+    {
+        question: "Who is the current state governor of Rivers State?",
+        answers: [
+            {option: "Beatrice Dokunbo", correct: false},
+            {option: "Tonye Cole", correct: false},
+            {option: "Nyesome Wike", correct: true}
+        ]
+    },
+    {
+        question: "who is the president Elect of Federal Republic of Nigeria?",
+        answers: [
+            {option: "Goodluck Jonathan", correct: false},
+            {option: "Bola Ahmed Tinubu", correct: true},
+            {option: "Peter Obi", correct: false},
+        ]
+    },
+    {
+        question: "what are the colors of the Nigerian flag",
+        answers: [
+            {option: "green white green", correct: true},
+            {option: "blue white red", correct: false},
+            {option: "black yellow white", correct: false},
+        ]
+    },
+    {
+        question: "what is the name of Nigerian Currency",
+        answers: [
+            {option: "dollars", correct: false},
+            {option: "naira", correct: true},
+            {option: "cedis", correct: false},
+        ]
+    },
+    {
+        question: "what is the fastest means of transportation",
+        answers: [
+            {option: "land", correct: false},
+            {option: "sea", correct: false},
+            {option: "air", correct: true},
+        ]
+    },
+    {
+        question: "How many children does wizkid have?",
+        answers: [
+            {option: "2", correct: false},
+            {option: "4", correct: true},
+            {option: "10", correct: false},
+        ]
+    },
+    {
+        question: "The organ for seeing is called ?",
+        answers: [
+            {option: "ear", correct: false},
+            {option: "nose", correct: false},
+            {option: "eye", correct: true},
+        ]
+    },
+    {
+        question: "what is the geo-political zone of Lagos",
+        answers: [
+            {option: "south-west", correct: true},
+            {option: "north-east", correct: false},
+            {option: "north-central", correct: false},
+        ]
+    },
+    {
+        question: "what is the gomycode building refered to?",
+        answers: [
+            {option: "community", correct: false},
+            {option: "hackerspace", correct: true},
+            {option: "trap house", correct: false},
+        ]
+    }
+
+    
+]
